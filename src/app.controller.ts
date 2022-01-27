d3f631dde4ff945e09021b5f940695256ee58d50
daegon-lee <daegon.lee@navercorp.com>
2022-01-27 12:05:46 +0900

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Render } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+  @Get()
+  @Render('index')
+  root() {
+    return {
+      data: {
+        title: 'Chattings',
+        copyright: 'daegon lee',
+      },
+    };
+  }
+
+  @Get('/status')
+  healthCheck() {
+    return 'OK';
+  }
+}
